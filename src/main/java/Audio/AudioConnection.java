8ed280806bbcae10b77e2c486506ae5940a2a610
AlienIdeology <wcliaw610@gmail.com>
2017-04-02 18:18:51 -0500

    rename connection

---
+++ src/main/java/Audio/AudioConnection.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Audio;
+
+import Command.*;
+import Config.*;
+import Main.*;
+
+import net.dv8tion.jda.core.entities.VoiceChannel;
+import net.dv8tion.jda.core.managers.AudioManager;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+
+/**
+ *
+ * @author liaoyilin
+ */
+public class AudioConnection {
+    
+    public static VoiceChannel vc;
+    public static AudioManager am;
+    
+    public static void connect(MessageReceivedEvent e)
+    {
+        try {
+            vc = e.getMember().getVoiceState().getChannel();
+            am = e.getGuild().getAudioManager();
+            am.openAudioConnection(vc);
+            
+            e.getChannel().sendMessage(Emoji.globe + " Joined Voice Channel `" + vc.getName() + "`").queue();
+        } catch (IllegalArgumentException iea) {
+            e.getChannel().sendMessage(Emoji.error + " You must connect to a voice channel first.").queue();
+        }
+    }
+    
+    public static void disconnect(MessageReceivedEvent e)
+    {
+            try {
+                am = e.getGuild().getAudioManager();
+                am.closeAudioConnection();
+            } catch (NullPointerException npe) {
+                e.getChannel().sendMessage(Emoji.error + " You are not in a voice channel.").queue();
+            }
+            
+            e.getChannel().sendMessage(Emoji.globe + " Left Voice Channel `" + vc.getName() + "`").queue();
+    }
+    
+    
+}
