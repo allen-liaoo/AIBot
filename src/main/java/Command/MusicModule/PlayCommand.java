4a12b0f37c0d2a36fc20c5538962f93a75a94abe
AlienIdeology <wcliaw610@gmail.com>
2017-04-04 07:34:05 -0500

    Add Music Module from AIBot Beta

---
+++ src/main/java/Command/MusicModule/PlayCommand.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Command.MusicModule;
+
+import Audio.AudioConnection;
+import Audio.Music;
+import Command.Command;
+import Config.Emoji;
+import Main.Main;
+import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
+import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
+import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import java.util.regex.Matcher;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+
+/**
+ *
+ * @author Alien Ideology <alien.ideology at alien.org>
+ */
+public class PlayCommand implements Command{
+
+    @Override
+    public boolean called(String[] args, MessageReceivedEvent e) {
+        return true;
+    }
+
+    @Override
+    public void help(MessageReceivedEvent e) {
+        
+    }
+
+    @Override
+    public void action(String[] args, MessageReceivedEvent e) {
+        if(args.length == 1 && "-h".equals(args[0])) 
+        {
+            help(e);
+        }
+        else
+        {
+            Music.play(args, e);
+        }
+    }
+
+    @Override
+    public void executed(boolean success, MessageReceivedEvent e) {
+        
+    }
+    
+}
