cdfa2ce51bf08a7938f69f06f00f5068b09b9d93
AlienIdeology <wcliaw610@gmail.com>
2017-04-04 17:53:57 -0500

    v1.2

--- src/main/java/Command/InformationModule/AboutCommand.java
+++ src/main/java/Command/InformationModule/AboutCommand.java
@@ -27,7 +27,7 @@ public class AboutCommand implements Command{
     
     public final static String BOT_DES = "The AIBot that started in the March of 2017 for music and fun commands.\n"
                                     + "**Developer:** Ayy™#3103 *aka* AlienIdeology, Allen Liao\n"
-                                    + "**Version:** 0.1.0\n"
+                                    + "**Version:** 0.1.2\n"
                                     + "**Annoucement:** Working on Music Module~~\n"
                                     + "**Latest Changes:** Reconstruct Help Message\n"
                                     + "**Github Link:** https://github.com/AlienIdeology/AIBot";
