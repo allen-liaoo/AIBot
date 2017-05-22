64c006c212659db71300efb5104787dca9288c84
Allen Liao <wcliaw610@gmail.com>
2017-05-22 10:41:19 -0500

    Writing PermCommand. Not finished Yet.
    !!Remember to add Green/Red Tick Emoji.

--- org.alienideology.aibot/src/main/java/command/information/PermCommand.java
+++ org.alienideology.aibot/src/main/java/command/information/PermCommand.java
@@ -1,9 +1,17 @@
 package command.information;
 
 import command.Command;
+import constants.Emoji;
+import utility.UtilBot;
 import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.entities.Role;
+import net.dv8tion.jda.core.Permission;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import setting.Prefix;
+
+import java.util.List;
+import java.util.ArrayList;
 
 /**
  * @author AlienIdeology
@@ -26,6 +34,50 @@ public class PermCommand extends Command {
 
     @Override
     public void action(String[] args, MessageReceivedEvent e) {
+        Member mem = null;
+        
+        if (args.length == 0) {     // Self
+            mem = e.getMember();
+        } else if (args[0].length == 18) {  // ID
+            mem = e.getGuild().getMemberById(args[0]);
+        } else if(!e.getMessage().getMentionedUsers().isEmpty()) {  // Mention
+            mem = e.getGuild().getMemebrById(e.getMessage().getMentionedUsers().get(0).getId());
+        }
+        
+        
 
     }
+    
+    private void sendMemberPerms(MessageReceivedEvent e, Member mem) {
+        StringBuilder message = new StringBuilder("Permissions for user **").append(mem.getEffectiveName())
+            .append("**#").append(mem.getUser().getDiscriminator()).append(":");
+        
+        for(Permission perm : Permission.values()) {
+            if(mem.hasPermissions(perm)) {
+                message.append(Emoji.GREEN_TICK);
+            } else {
+                message.append(Emoji.RED_TICK);
+            }
+            message.append(perm.getName()).append("\n");
+        }
+        
+        e.getChannel().sendMessage(message.toString());
+    }
+    
+    private void sendRolePerms(MessageReceivedEvent e, Role role) {
+        StringBuilder message = new StringBuilder("Permissions for role **").append(role.getName())
+            .append("** ").append("(Position: ").append(role.getPosition()).append(") :");
+        
+        for(Permission perm : Permission.values()) {
+            if(mem.hasPermissions(perm)) {
+                message.append(Emoji.GREEN_TICK);
+            } else {
+                message.append(Emoji.RED_TICK);
+            }
+            message.append(perm.getName()).append("\n");
+        }
+        
+        e.getChannel().sendMessage(message.toString());
+    }
+    
 }
